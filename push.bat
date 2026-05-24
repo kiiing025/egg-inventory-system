b2208b02c0e00301b9ee39978ae37a081e128bbd
King Josh <robles.kingjosh0@gmail.com>
2026-05-24 09:04:48 -0700

    commit egg tracker changes

---
+++ push.bat
@@ -0,0 +1,6 @@
+@echo off
+cd /d "c:\Users\User\OneDrive\Desktop\egg-inventory-system"
+git add index.html
+git commit -m "Rebrand website to Yolk with simplified header"
+git push origin main
+pause
